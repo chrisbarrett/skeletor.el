cb86de997e0d42a84348c191fdee31fcf7d7e5c2
Chris Barrett <chris.d.barrett@me.com>
2013-12-17 16:43:46 +1300

    add project skeletons

---
+++ project-skeletons/elisp-package/__PROJECT-NAME__.el
@@ -0,0 +1,33 @@
+;;; __PROJECT-NAME__.el --- __DESCRIPTION__
+
+;; Copyright (C) __YEAR__ __USER-NAME__
+
+;; Author: __USER-NAME__ <__USER-MAIL-ADDRESS__>
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; __DESCRIPTION__
+
+;;; Code:
+
+
+
+(provide '__PROJECT-NAME__)
+
+;;; __PROJECT-NAME__.el ends here
